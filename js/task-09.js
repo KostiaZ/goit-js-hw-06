5146215cd94f46bf45968b72b3fd2e4d1425eeb5
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2022-12-11 23:38:12 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const button = document.querySelector('.change-color');
+const bodyColor = document.querySelector('body');
+const codeColor = document.querySelector('.color');
+button.addEventListener('click', getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const newColor =
+    '#' +
+    Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0);
+  bodyColor.style.backgroundColor = newColor;
+  codeColor.textContent = newColor;
 }
