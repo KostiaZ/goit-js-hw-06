e50d74aa98975929a30fe56443db9086b76c59fe
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2022-12-11 22:14:24 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', getData);
+function getData(evt) {
+  const {
+    elements: { email, password },
+  } = evt.currentTarget;
+
+  evt.preventDefault();
+
+  if (email.value === '' || password.value === '') {
+    alert('Please, fill all fields of the form!');
+  } else {
+    const data = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(data);
+  }
+  evt.currentTarget.reset();
+}
